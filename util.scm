1e3c819863be9b277fee770de028deb37b7b1225
John Croisant <john@croisant.net>
2012-08-18 23:29:14 -0400

    Added %protolk-util module with make-exception and raise procedures.

---
+++ util.scm
@@ -0,0 +1,50 @@
+;;
+;; Protolk: flexible Scheme objects with message passing and prototypes
+;;
+;; Copyright © 2012  John Croisant.
+;; All rights reserved.
+;;
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions
+;; are met:
+;;
+;; - Redistributions of source code must retain the above copyright
+;;   notice, this list of conditions and the following disclaimer.
+;;
+;; - Redistributions in binary form must reproduce the above copyright
+;;   notice, this list of conditions and the following disclaimer in
+;;   the documentation and/or other materials provided with the
+;;   distribution.
+;;
+;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;; "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;; LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+;; FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+;; COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+;; INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+;; (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+;; SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+;; HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+;; STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+;; OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+;;; This module contains utility procedures and/or syntax used in the
+;;; Protolk source code itself. These are NOT part of the Protolk API,
+;;; and may change at any time. Do not use them.
+
+(module %protolk-util *
+(import scheme chicken)
+
+
+(define (make-exception kind message . other-properties)
+  (make-composite-condition
+   (make-property-condition 'exn 'message message)
+   (apply make-property-condition kind other-properties)))
+
+(define (raise kind message . other-properties)
+  (abort (apply make-exception kind message other-properties)))
+
+
+) ;; end  module %protolk-util
