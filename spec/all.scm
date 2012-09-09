5024fc739f41f571e98720b5b0544275495fb247
John Croisant <john@croisant.net>
2012-09-09 02:21:35 -0400

    Moved specs back into spec directory.
    tests/run.scm (used by chicken-install) loads spec/all.scm

---
+++ spec/all.scm
@@ -0,0 +1,17 @@
+
+(register-feature! 'protolk-all-tests)
+
+(load-relative "helpers")
+
+(load-relative "../protolk-internal.scm")
+(load-relative "../protolk-primitives.scm")
+(load-relative "../protolk-stdpob.scm")
+(load-relative "../protolk.scm")
+
+(import protolk-internal protolk-primitives protolk)
+
+(load-relative "primitives-spec.scm")
+(load-relative "protolk-spec.scm")
+(load-relative "stdpob-spec.scm")
+
+(test-exit)
--- tests/run.scm
+++ tests/run.scm
@@ -1,17 +1,2 @@
 
-(register-feature! 'protolk-all-tests)
-
-(load-relative "helpers")
-
-(load-relative "../protolk-internal.scm")
-(load-relative "../protolk-primitives.scm")
-(load-relative "../protolk-stdpob.scm")
-(load-relative "../protolk.scm")
-
-(import protolk-internal protolk-primitives protolk)
-
-(load-relative "primitives-spec.scm")
-(load-relative "protolk-spec.scm")
-(load-relative "stdpob-spec.scm")
-
-(test-exit)
+(load-relative "../spec/all.scm")
